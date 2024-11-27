84825df7f3f408fc89d1d04f04dba328234990ec
Muntazir-sd <sayedmuntazir007@gmail.com>
2024-11-27 19:24:57 +0530

    Added Roboto Font from google font.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -11,6 +12,16 @@ const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
+});
+
+const roboto = Roboto({
+  // https://fonts.google.com/knowledge/glossary/subsetting
+  subsets: ["latin"],
+  variable: "--font-roboto",
+  // weight: '400': A string for a single weight value - for the font Inter, the possible values are '100', '200', '300', '400', '500', '600', '700', '800', '900' or 'variable' where 'variable' is the default)
+  // weight: '100 900': A string for the range between 100 and 900 for a variable font
+  // weight: ['100','400','900']: An array of 3 possible values for a non variable font
+  weight: ["100", "300", "400", "500", "700", "900"],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +36,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${roboto.variable}`}
+      >
         {children}
       </body>
     </html>
